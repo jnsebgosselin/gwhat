e75d2a751dfbc445ade7379a32c7d17cdc3943c3
Jean-Sébastien Gosselin <jnsebgosselin@gmail.com>
2014-12-30 23:04:29 -0500

    Updated licence

--- WHAT/hydroprint.py
+++ WHAT/hydroprint.py
@@ -1,8 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Dec  7 07:46:47 2014
+Copyright 2014 Jean-Sebastien Gosselin
 
-@author: jsgosselin
+email: jnsebgosselin@gmail.com
+
+This file is part of WHAT (Well Hydrograph Analysis Toolbox).
+
+WHAT is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
 """
 
 #----- STANDARD LIBRARY IMPORTS -----
