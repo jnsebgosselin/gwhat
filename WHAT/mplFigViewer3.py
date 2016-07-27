23ec98fc0f72da7dffb3fc11e40c03098528d6eb
jnsebgosselin <jnsebgosselin@gmail.com>
2016-07-27 14:25:50 -0400

    mplFigViewer cleanup

--- WHAT/mplFigViewer3.py
+++ WHAT/mplFigViewer3.py
@@ -48,48 +48,44 @@ WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 #---- THIRD PARTY IMPORTS ----
 
 from PySide import QtGui, QtCore
-import time
-import copy
 
-#==============================================================================   
-class MplViewer(QtGui.QFrame):                                    # MplViewer #
-#==============================================================================
+
+class MplViewer(QtGui.QFrame):  # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     def __init__(self, parent=None):
         super(MplViewer, self).__init__(parent)
-        
-        self.setFrameStyle(QtGui.QFrame.Panel|QtGui.QFrame.Raised)
+
         self.setLineWidth(2)
         self.setMidLineWidth(1)
         self.setStyleSheet("background-color: white")
-        
+
         self.img = []
         self.qpix_buff = []
-        
-    def load_mpl_figure(self, mplfig, view_dpi): #========= Load Image ====
-        
+
+    def load_mpl_figure(self, mplfig, view_dpi):  # ===========================
+
         self.qpix_buff = []
-        
+
         #----------------------------------------------------- figure size ----
-        
+
         bbox = mplfig.get_window_extent()
-        self.fwidth  = bbox.width
+        self.fwidth = bbox.width
         self.fheight = bbox.height
-        
+
         #------------------------------------------- save figure to buffer ----
-        
+
         # http://stackoverflow.com/questions/8598673/
         # how-to-save-a-pylab-figure-into-in-memory-file-which-can-be-read
         # -into-pil-image/8598881#8598881
-        
+
         # http://stackoverflow.com/questions/1300908/
         # load-blob-image-data-into-qpixmap
-                    
+
         #-- print mpl figure to buffer_rgba --
 
         # scale dpi of figure to view_dpi
-        orig_fig_dpi = mplfig.get_dpi()            
-        mplfig.dpi = view_dpi 
+        orig_fig_dpi = mplfig.get_dpi()
+        mplfig.dpi = view_dpi
         # propagate changes to renderer
         mplfig.canvas.draw()
         renderer = mplfig.canvas.get_renderer()
@@ -98,105 +94,103 @@ class MplViewer(QtGui.QFrame):                                    # MplViewer #
         # generate img buffer
         imgbuf = mplfig.canvas.buffer_rgba()
         imgwidth = int(renderer.width)
-        imgheight =int(renderer.height)
+        imgheight = int(renderer.height)
         # restore fig and renderer dpi
         renderer.dpi = orig_ren_dpi
         mplfig.dpi = orig_fig_dpi
-        
+
         #-- convert buffer to QPixmap --
 
         self.img = QtGui.QImage(imgbuf, imgwidth, imgheight,
                                 QtGui.QImage.Format_ARGB32)
         self.img = QtGui.QImage.rgbSwapped(self.img)
         self.img = QtGui.QPixmap(self.img)
-            
-        self.repaint() 
-                        
-    def paintEvent(self, event): #============================= paintEvent ====
+
+        self.repaint()
+
+    def paintEvent(self, event):  # ===========================================
         super(MplViewer, self).paintEvent(event)
-        
+
         qp = QtGui.QPainter()
         qp.begin(self)
-        
+
         #---- prepare paint rect ----
-        
+
         fw = self.frameWidth()
         rect = QtCore.QRect(0 + fw, 0 + fw,
                             self.size().width() - 2 * fw,
-                            self.size().height() - 2 * fw)            
-        
+                            self.size().height() - 2 * fw)
+
         #----- check/update image buffer ---
-        
+
         qpix2print = None
         for qpix in self.qpix_buff:
-            if qpix.size().width() == rect.width():                    
-                qpix2print = qpix                     
+            if qpix.size().width() == rect.width():
+                qpix2print = qpix
                 break
 
         if qpix2print == None:
-            qpix2print = self.img.scaledToWidth(rect.width(),
-                             mode=QtCore.Qt.SmoothTransformation)
+            qpix2print = self.img.scaledToWidth(
+                rect.width(), mode=QtCore.Qt.SmoothTransformation)
             self.qpix_buff.append(qpix2print)
-        
+
         #---- draw pixmap ----
-        
-#        qp.setRenderHint(QtGui.QPainter.Antialiasing, True) 
-        qp.drawPixmap(rect, qpix2print) 
-            
+
+#        qp.setRenderHint(QtGui.QPainter.Antialiasing, True)
+        qp.drawPixmap(rect, qpix2print)
+
         qp.end()
-        
-        
-#==============================================================================
-class ImageViewer(QtGui.QScrollArea):                           # ImageViewer #
-#==============================================================================            
-   
+
+
+class ImageViewer(QtGui.QScrollArea):  # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
     """
-    This is a PySide widget class to display a matplotlib figure image in a 
+    This is a PySide widget class to display a matplotlib figure image in a
     QScrollArea with zooming and panning capability with CTRL + Mouse_wheel
     and Left-click event.
     """
-    
+
     def __init__(self, parent=None):
-        super(ImageViewer, self).__init__(parent)    
-        
+        super(ImageViewer, self).__init__(parent)
+
         self.setWindowTitle('Image Viewer')
         self.setAlignment(QtCore.Qt.AlignCenter)
-        
-        #---- init. variable ----        
-        
+
+        #---- init. variable ----
+
         self.scaleFactor = 0
         self.scaleStep = 1.2
         self.pan = False
-        
+
         #---- image container Set Up ----
-        
+
         self.imageCanvas = MplViewer()
-                
+
         self.imageCanvas.installEventFilter(self)
         self.setWidget(self.imageCanvas)
-             
-    def eventFilter(self, widget, event): #=================== eventFilter ==== 
-        
+
+    def eventFilter(self, widget, event):  # ==================================
+
         # http://stackoverflow.com/questions/17525608/
         # event-filter-cannot-intercept-wheel-event-from-qscrollarea
-        
+
         # http://stackoverflow.com/questions/20420072/
         # pyside-keypressevent-catching-enter-or-return
-        
+
         # http://stackoverflow.com/questions/19113532/
         # qgraphicsview-zooming-in-and-out-under-mouse-position
         # -using-mouse-wheel
 
-        #------------------------------------------------------------ ZOOM ----
-        
+        # ZOOM ----------------------------------------------------------------
+
         if event.type() == QtCore.QEvent.Type.Wheel:
-                               
+
             # http://stackoverflow.com/questions/8772595/
             # how-to-check-if-a-key-modifier-is-pressed-shift-ctrl-alt
-            
+
             modifiers = QtGui.QApplication.keyboardModifiers()
-                      
-            if modifiers == QtCore.Qt.ControlModifier:                
+
+            if modifiers == QtCore.Qt.ControlModifier:
                 if event.delta() > 0:
                     self.zoomIn()
                 else:
@@ -204,129 +198,119 @@ class ImageViewer(QtGui.QScrollArea):                           # ImageViewer #
                 return True
             else:
                 return False
-        
-        #------------------------------------------------------------ PAN ----
-        
-        #---- Set ClosedHandCursor ----
-        
+
+        # PAN -----------------------------------------------------------------
+
+        # Set ClosedHandCursor:
+
         elif event.type() == QtCore.QEvent.Type.MouseButtonPress:
-                  
+
             if event.button() == QtCore.Qt.MouseButton.LeftButton:
-                
+
                 QtGui.QApplication.setOverrideCursor(
-                                                    QtCore.Qt.ClosedHandCursor)
+                    QtCore.Qt.ClosedHandCursor)
                 self.pan = True
                 self.xclick = event.globalX()
                 self.yclick = event.globalY()
-        
-        #---- Reset Cursor ----
+
+        # Reset Cursor:
 
         elif event.type() == QtCore.QEvent.Type.MouseButtonRelease:
-            
+
             QtGui.QApplication.restoreOverrideCursor()
             self.pan = False
-        
-        #---- Move  ScrollBar----
-        
+
+        # Move  ScrollBar:
+
         elif event.type() == QtCore.QEvent.Type.MouseMove:
-            
+
             if self.pan == True:
-                
+
                 dx = self.xclick - event.globalX()
                 self.xclick = event.globalX()
-                
+
                 dy = self.yclick - event.globalY()
                 self.yclick = event.globalY()
-                
+
                 scrollBarH = self.horizontalScrollBar()
                 scrollBarH.setValue(scrollBarH.value() + dx)
-    
+
                 scrollBarV = self.verticalScrollBar()
                 scrollBarV.setValue(scrollBarV.value() + dy)
 
         return QtGui.QWidget.eventFilter(self, widget, event)
 
-    def zoomIn(self):
+    def zoomIn(self):  # ======================================================
         if self.scaleFactor < 3:
             self.scaleFactor += 1
             self.scale_image()
             self.adjust_scrollbar(self.scaleStep)
 
-    def zoomOut(self):
+    def zoomOut(self):  # =====================================================
         if self.scaleFactor > -3:
             self.scaleFactor -= 1
             self.scale_image()
             self.adjust_scrollbar(1/self.scaleStep)
-                
-    def scale_image(self):
-        
-        new_width = int(self.imageCanvas.fwidth * 
+
+    def scale_image(self):  # =================================================
+
+        new_width = int(self.imageCanvas.fwidth *
                         self.scaleStep ** self.scaleFactor)
-        new_height = int(self.imageCanvas.fheight * 
+        new_height = int(self.imageCanvas.fheight *
                          self.scaleStep ** self.scaleFactor)
 
         self.imageCanvas.setFixedSize(new_width, new_height)
-       
-    def load_mpl_figure(self, mplfig, view_dpi=150):
+
+    def load_mpl_figure(self, mplfig, view_dpi=150):  # =======================
         self.imageCanvas.load_mpl_figure(mplfig, view_dpi)
         self.scale_image()
-        
-    def reset_original_image(self):
+
+    def reset_original_image(self):  # ========================================
         self.scaleFactor = 0
         self.scale_image()
 
-    def adjust_scrollbar(self, f):
-        
+    def adjust_scrollbar(self, f):  # =========================================
+
         #---- Adjust HScrollBar ----
-        
+
         hb = self.horizontalScrollBar()
         hb.setValue(int(f * hb.value() + ((f - 1) * hb.pageStep()/2)))
-                                
+
         #---- Adjust VScrollBar ----
-                                
-        vb = self.verticalScrollBar()                        
+
+        vb = self.verticalScrollBar()
         vb.setValue(int(f * vb.value() + ((f - 1) * vb.pageStep()/2)))
-                                
-                                
-if __name__ == '__main__':
+
+
+if __name__ == '__main__':  # =================================================
 
     import sys
     import matplotlib.pyplot as plt
+    plt.ioff()
     import numpy as np
 
     app = QtGui.QApplication(sys.argv)
-    
-    #---------------------------------------------- generate a mpl figure ----
-    
-    #---- generate data ----
-    
+
+    # generate a mpl figure ---------------------------------------------------
+
+    # generate data:
+
     N = 150
     x = np.random.rand(N)
     y = np.random.rand(N)
     colors = np.random.rand(N)
     area = np.pi * (15 * np.random.rand(N)) ** 2
-    
-    #---- setup figure and plot data ----
-    
-    fig = plt.figure()
-    
-    ax = fig.add_axes([0.075, 0.075, 0.9, 0.9])
-    fig.patch.set_facecolor('white')
-    fig.set_size_inches(6, 6)
-    
-    fbbox = fig.get_window_extent()
-    
+
+    # setup figure and plot data:
+
+    fig, ax = plt.subplots(facecolor='white', figsize=(8, 8))
     ax.scatter(x, y, s=area, c=colors, alpha=0.5)
-    
-    #------------------------------------------------------- image viewer ----
-    
+
+    # display it in a image viewer --------------------------------------------
+
     imageViewer = ImageViewer()
-    imageViewer.show()
-    
-    bbox = fig.get_window_extent()  
-    imageViewer.resize(bbox.width*1.2, bbox.height*1.2)
-    
-    fig.canvas.draw()
+    imageViewer.setFixedSize(450, 450)
     imageViewer.load_mpl_figure(fig)
-      
+    imageViewer.show()
+
     sys.exit(app.exec_())
