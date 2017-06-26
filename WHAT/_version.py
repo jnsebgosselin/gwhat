cb72e2c196d39f673a3617b9335131ba2bef74ee
Jean-Sébastien Gosselin <jnsebgosselin@gmail.com>
2017-06-26 13:12:48 -0400

    Updated version for release 4.2.0-Beta2 release

--- WHAT/_version.py
+++ WHAT/_version.py
@@ -5,5 +5,5 @@ Created on Sat Apr  8 09:11:32 2017
 @author: jnsebgosselin
 """
 
-__version__ = 'WHAT 4.2.0-Beta'
-__date__ = '31/01/2017'
+__version__ = 'WHAT 4.2.0-Beta2'
+__date__ = '06/26/2017'
