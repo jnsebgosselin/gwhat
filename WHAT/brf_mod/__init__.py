8da751ecb93ea0e766315cfba55094eb990790f2
Jean-Sébastien Gosselin <jnsebgosselin@gmail.com>
2017-06-28 22:50:07 -0400

    Revamp the brf module
    - new window to show current and previous BRF results
    - brf is now saved in project database

---
+++ WHAT/brf_mod/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+>>> import brf_mod as bm
+"""
+
+from brf_mod.kgs_brf import (produce_BRFInputtxt, produce_par_file, run_kgsbrf,
+                             read_BRFOutput)
+
+from brf_mod.kgs_gui import BRFMainWindow
+
+__version__ = '1.2.0'
