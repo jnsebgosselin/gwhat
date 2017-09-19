00818050289996d82808f1295bb4b16124af72c1
Jean-Sébastien Gosselin <jnsebgosselin@gmail.com>
2017-09-18 23:06:29 -0400

    Test download outside of qthread for coveralls

--- WHAT/tests/test_dwnld_weather_data.py
+++ WHAT/tests/test_dwnld_weather_data.py
@@ -14,11 +14,16 @@ from PyQt5.QtCore import Qt
 
 # Local imports
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-from meteo.dwnld_weather_data import DwnldWeatherWidget                # nopep8
+from meteo.dwnld_weather_data import DwnldWeatherWidget, RawDataDownloader
 
 
 # Qt Test Fixtures
 # --------------------------------
+
+@pytest.fixture
+def raw_downloader_bot(qtbot):
+    dwnld_worker = RawDataDownloader()
+    return dwnld_worker, qtbot
 
 
 @pytest.fixture
@@ -83,6 +88,22 @@ def test_load_stationlist(downloader_bot):
     # Assert that the data are stored correctly in the widget table.
     list_from_table = wxdata_downloader.station_table.get_staList()
     assert list_from_table == expected_result
+
+
+@pytest.mark.run(order=3)
+def test_download_raw_data(raw_downloader_bot):
+    dwnld_worker, qtbot = raw_downloader_bot
+
+    # Download data for station Marieville
+    projetpath = os.path.join(os.getcwd(), "@ new-prô'jèt!")
+    dwnld_worker.dirname = os.path.join(projetpath, 'Meteo', 'Raw')
+    dwnld_worker.StaName = "MARIEVILLE"
+    dwnld_worker.stationID = "5406"
+    dwnld_worker.yr_start = "2000"
+    dwnld_worker.yr_end = "2005"
+    dwnld_worker.climateID = "7024627"
+
+    dwnld_worker.download_data()
 
 
 @pytest.mark.run(order=3)
