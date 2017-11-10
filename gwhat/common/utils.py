2615d1154fa35a84c88918a31cb09ac66f6d492f
Jean-Sébastien Gosselin <jnsebgosselin@gmail.com>
2017-11-10 09:31:28 -0500

    Converted to a array based function (instead of float)

--- gwhat/common/utils.py
+++ gwhat/common/utils.py
@@ -11,30 +11,19 @@
 import numpy as np
 
 
-def latlon_to_dist(lat1, lon1, lat2, lon2):
+def calc_dist_from_coord(lat1, lon1, lat2, lon2):
     """
-    Computes the horizontal distance in km between 2 points from geographic
-    coordinates given in decimal degrees.
-
-    source:
-    www.stackoverflow.com/questions/19412462 (last accessed on 17/01/2014)
+    Compute the  horizontal distance in km between a location given in
+    decimal degrees and a set of locations also given in decimal degrees.
     """
-    from math import sin, cos, sqrt, atan2, radians
+    lat1, lon1 = np.radians(lat1), np.radians(lon1)
+    lat2, lon2 = np.radians(lat2), np.radians(lon2)
 
-    r = 6373  # r is the Earth radius in km.
+    r = 6373  # r is the Earth radius in km
 
-    # Convert decimal degrees to radians.
-    lat1 = radians(lat1)
-    lon1 = radians(lon1)
-    lat2 = radians(lat2)
-    lon2 = radians(lon2)
-
-    # Compute the horizontal distance between the two points in km.
     dlon = lon2 - lon1
     dlat = lat2 - lat1
-    a = (sin(dlat/2))**2 + cos(lat1) * cos(lat2) * (sin(dlon/2))**2
-    c = 2 * atan2(np.sqrt(a), sqrt(1-a))
+    a = np.sin(dlat/2)**2 + np.cos(lat1)*np.cos(lat2)*np.sin(dlon/2)**2
+    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
 
-    dist = r * c
-
-    return dist
+    return r * c
