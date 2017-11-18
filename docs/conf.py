5cd0e9569f468ea2b51af2b83674be4f28f12518
Jean-Sébastien Gosselin <jnsebgosselin@gmail.com>
2017-11-18 10:49:34 -0500

    extra css try#4

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,11 @@
 # sys.path.insert(0, os.path.abspath('.'))
 import sphinx_rtd_theme
 
+
+def setup(app):
+    app.add_stylesheet('extra.css')
+
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
