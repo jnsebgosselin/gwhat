d2e76375bbe9494e5241d56e83edeb62d7106ab5
Jean-Sébastien Gosselin <jnsebgosselin@gmail.com>
2017-11-30 17:00:18 -0500

    Added tests to test Add PET feature.

--- gwhat/tests/test_gapfill_weather_gui.py
+++ gwhat/tests/test_gapfill_weather_gui.py
@@ -18,8 +18,9 @@ from PyQt5.QtCore import Qt
 
 # Local imports
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-from gwhat.meteo.gapfill_weather_gui import GapFillWeatherGUI
+from gwhat.meteo.gapfill_weather_gui import GapFillWeatherGUI, QFileDialog
 from gwhat.common.utils import delete_folder_recursively
+from gwhat.meteo.weather_reader import read_weather_datafile
 
 
 # Qt Test Fixtures
@@ -103,7 +104,55 @@ def test_delete_data(gapfill_weather_bot, mocker):
 
 
 @pytest.mark.run(order=5)
-def test_gapfill_all_data(gapfill_weather_bot, mocker):
+def test_fill_data(gapfill_weather_bot):
+    """
+    Fill the data for the first dataset.
+    """
+    delete_folder_recursively(output_dir)
+
+    gapfiller, qtbot = gapfill_weather_bot
+    gapfiller.show()
+    qtbot.mouseClick(gapfiller.btn_refresh_staList, Qt.LeftButton)
+
+    # Gapfill the data for each dataset in batch.
+    qtbot.mouseClick(gapfiller.btn_fill, Qt.LeftButton)
+
+    # Assert that all the ouput files were generated correctly.
+    basenames = ["IBERVILLE (7023270)_2000-2010.out",
+                 "IBERVILLE (7023270)_2000-2010.log",
+                 "weather_normals.pdf"]
+    for basename in basenames:
+        fname = os.path.join(output_dir, "IBERVILLE (7023270)", basename)
+        assert os.path.exists(fname)
+
+    # Assert that ETP was NOT added to the output file.
+    filename = os.path.join(output_dir, "IBERVILLE (7023270)", basenames[0])
+    wxdf = read_weather_datafile(filename)
+    assert wxdf['PET'] is None
+
+
+@pytest.mark.run(order=5)
+def test_add_ETP(gapfill_weather_bot, mocker):
+    gapfiller, qtbot = gapfill_weather_bot
+    gapfiller.show()
+    qtbot.mouseClick(gapfiller.btn_refresh_staList, Qt.LeftButton)
+
+    # Mock the QFileDialog to return the path of the file.
+    filename = os.path.join(output_dir, "IBERVILLE (7023270)",
+                            "IBERVILLE (7023270)_2000-2010.out")
+    mocker.patch.object(QFileDialog, 'getOpenFileName',
+                        return_value=(filename, '*.out'))
+
+    # Add ETP to file to the output weather data file.
+    qtbot.mouseClick(gapfiller.btn_add_PET, Qt.LeftButton)
+
+    # Assert that ETP was added to the output file.
+    wxdf = read_weather_datafile(filename)
+    assert len(wxdf['PET']) == len(wxdf['Time'])
+
+
+@pytest.mark.run(order=5)
+def test_gapfill_all_data(gapfill_weather_bot):
     """
     Fill the data in each dataset one by one with the default values for
     the parameters.
@@ -114,23 +163,42 @@ def test_gapfill_all_data(gapfill_weather_bot, mocker):
     gapfiller.show()
     qtbot.mouseClick(gapfiller.btn_refresh_staList, Qt.LeftButton)
 
+    # Check the option "Add PET to datafile" in the "Advanced Settings".
+    gapfiller.add_PET_ckckbox(Qt.Checked)
+
     # Gapfill the data for each dataset in batch
     qtbot.mouseClick(gapfiller.btn_fill_all, Qt.LeftButton)
     qtbot.waitUntil(lambda: not gapfiller.isFillAll_inProgress, timeout=100000)
 
     # Assert that all the ouput files were generated correctly.
-    files = [os.path.join(output_dir, "IBERVILLE (7023270)", "IBERVILLE (7023270)_2000-2010.out"),
-             os.path.join(output_dir, "IBERVILLE (7023270)", "IBERVILLE (7023270)_2000-2010.log"),
-             os.path.join(output_dir, "IBERVILLE (7023270)", "weather_normals.pdf"),
-             os.path.join(output_dir, "L'ACADIE (702LED4)", "L'ACADIE (702LED4)_2000-2010.out"),
-             os.path.join(output_dir, "L'ACADIE (702LED4)", "L'ACADIE (702LED4)_2000-2010.log"),
-             os.path.join(output_dir, "L'ACADIE (702LED4)", "weather_normals.pdf"),
-             os.path.join(output_dir, "MARIEVILLE (7024627)", "MARIEVILLE (7024627)_2000-2010.out"),
-             os.path.join(output_dir, "MARIEVILLE (7024627)", "MARIEVILLE (7024627)_2000-2010.log"),
-             os.path.join(output_dir, "MARIEVILLE (7024627)", "weather_normals.pdf")
+    files = [os.path.join(output_dir, "IBERVILLE (7023270)",
+                          "IBERVILLE (7023270)_2000-2010.out"),
+             os.path.join(output_dir, "IBERVILLE (7023270)",
+                          "IBERVILLE (7023270)_2000-2010.log"),
+             os.path.join(output_dir, "IBERVILLE (7023270)",
+                          "weather_normals.pdf"),
+             os.path.join(output_dir, "L'ACADIE (702LED4)",
+                          "L'ACADIE (702LED4)_2000-2010.out"),
+             os.path.join(output_dir, "L'ACADIE (702LED4)",
+                          "L'ACADIE (702LED4)_2000-2010.log"),
+             os.path.join(output_dir, "L'ACADIE (702LED4)",
+                          "weather_normals.pdf"),
+             os.path.join(output_dir, "MARIEVILLE (7024627)",
+                          "MARIEVILLE (7024627)_2000-2010.out"),
+             os.path.join(output_dir, "MARIEVILLE (7024627)",
+                          "MARIEVILLE (7024627)_2000-2010.log"),
+             os.path.join(output_dir, "MARIEVILLE (7024627)",
+                          "weather_normals.pdf")
              ]
     for file in files:
         assert os.path.exists(file)
+
+    # Assert that ETP was added to the output file.
+    filenames = files[0, 3, 6]
+    for filename in filenames:
+        wxdf = read_weather_datafile(filename)
+        assert len(wxdf['PET']) == len(wxdf['Time'])
+
 
 if __name__ == "__main__":
     pytest.main(['-x', os.path.basename(__file__), '-v', '-rw'])
