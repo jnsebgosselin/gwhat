d231d0720a08ac32c155aca4dcb0218d84acd4d9
Jean-Sébastien Gosselin <jnsebgosselin@gmail.com>
2017-12-11 17:13:43 -0500

    PR: Update the "About GWHAT" window and reduce startup time. (#120)
    * About GWHAT window created on demand only
    
    Instead of creating the GWHAT window on startup, it is created only when
    the button about gwhat is clicked.
    
    * Updated the text
    
    * Use project url from __init__

--- gwhat/tests/test_tabwidget.py
+++ gwhat/tests/test_tabwidget.py
@@ -12,9 +12,9 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 # ---- Third parties imports
 
-import pytest                                                          # nopep8
-from PyQt5.QtCore import Qt                                            # nopep8
-from PyQt5.QtWidgets import QWidget                                    # nopep8
+import pytest
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget
 
 # ---- Local imports
 
@@ -31,8 +31,7 @@ def tabwidget_bot(qtbot):
     tabwidget.addTab(QWidget(), 'Tab#1')
     tabwidget.addTab(QWidget(), 'Tab#2')
     tabwidget.addTab(QWidget(), 'Tab#3')
-
-    tabwidget.aboutwhat.setModal(False)
+    tabwidget._pytesting = True
 
     qtbot.addWidget(tabwidget)
 
@@ -43,12 +42,20 @@ def tabwidget_bot(qtbot):
 
 
 def test_tabwidget_and_about_window(tabwidget_bot):
+    """Test the showing and closing of the About GWHAT window."""
     tabwidget, qtbot = tabwidget_bot
     tabwidget.show()
 
+    assert tabwidget.about_win is None
+
+    # Show about window and assert it was created and showed correctly.
     qtbot.mouseClick(tabwidget.about_btn, Qt.LeftButton)
-    assert tabwidget.aboutwhat.isVisible()
-    tabwidget.aboutwhat.close()
+    assert tabwidget.about_win
+    assert tabwidget.about_win.isVisible()
+
+    # Close the about window and assert it was closed correctly.
+    qtbot.mouseClick(tabwidget.about_win.ok_btn, Qt.LeftButton)
+    assert not tabwidget.about_win.isVisible()
 
 
 def test_tabwidget_index_memory(tabwidget_bot):
