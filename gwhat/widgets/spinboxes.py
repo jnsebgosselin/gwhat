2758b9fcd306ed324fb51a19f98f92a9a894cfe6
Jean-Sébastien Gosselin <jnsebgosselin@gmail.com>
2018-05-16 00:35:22 -0400

    Create a SpinBox to show a list of strings

---
+++ gwhat/widgets/spinboxes.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+# Copyright © 2014-2018 GWHAT Project Contributors
+# https://github.com/jnsebgosselin/gwhat
+#
+# This file is part of GWHAT (Ground-Water Hydrograph Analysis Toolbox).
+# Licensed under the terms of the GNU General Public License.
+
+# ---- Imports: Standard Libraries
+
+import sys
+import os
+import os.path as osp
+from abc import abstractmethod
+
+# ---- Imports: Third Parties
+
+from PyQt5.QtWidgets import (QApplication, QDoubleSpinBox, QFileDialog,
+                             QGridLayout, QListWidget, QMenu, QMessageBox,
+                             QStyle, QToolButton, QWidget, QSpinBox,
+                             QLineEdit)
+
+
+class StrSpinBox(QSpinBox):
+
+    def __init__(self, model=None):
+        super(StrSpinBox, self).__init__()
+        self.set_model(model)
+        self.setValue(0)
+        self.setWrapping(True)
+        self.lineEdit().setReadOnly(True)
+
+    @property
+    def model(self):
+        """Return the data model of the spin box."""
+        return self.__model
+
+    def set_model(self, model):
+        """Set the data model of the spin box."""
+        if model is None:
+            self.__model = None
+            self.setMaximum(0)
+        else:
+            if isinstance(model, list):
+                self.__model = model
+                self.setMaximum(len(model)-1)
+            else:
+                raise ValueError("The model must be a dict")
+        self.setEnabled(self.__model is not None)
+
+        # Force a refresh of the spinbox when the model changes :
+        self.setValue(self.value())
+
+    def textFromValue(self, value):
+        """Qt method override."""
+        if self.model is not None:
+            return str(self.model[value])
+        else:
+            return ' '
+
+
+# %% if __name__ == '__main__'
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    model = {'test': 'patate', 'test2': 'orange'}
+    text_sp = DictSpinBox(model)
+    text_sp.show()
+
+    sys.exit(app.exec_())
