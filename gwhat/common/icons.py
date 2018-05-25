0d4175e10857fd28fdadc0f575f5c210a436788c
Jean-Sébastien Gosselin <jnsebgosselin@gmail.com>
2018-05-25 11:39:07 -0400

    New icon fill_weather

--- gwhat/common/icons.py
+++ gwhat/common/icons.py
@@ -88,7 +88,8 @@ ICON_NAMES = {'master': 'WHAT',
               'zoom_to_rect': 'zoom_to_rect',
               'show_glue_wl': 'show_glue_wl',
               'show_meteo': 'show_meteo',
-              'language': 'language'}
+              'language': 'language',
+              'fill_weather': 'fill_weather'}
 
 ICON_SIZES = {'large': (32, 32),
               'normal': (28, 28),
