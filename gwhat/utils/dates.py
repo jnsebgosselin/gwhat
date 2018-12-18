e72bc4d7ecbd6dcd8301ad8c6cb16e4bea4c22ec
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2018-12-18 13:53:37 -0500

    Add qdate_from_xldate in utils/date.py
    Conver an numerical Excel date to a QDate object

---
+++ gwhat/utils/dates.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Copyright © GWHAT Project Contributors
+# https://github.com/jnsebgosselin/gwhat
+#
+# This file is part of GWHAT (Ground-Water Hydrograph Analysis Toolbox).
+# Licensed under the terms of the GNU General Public License.
+
+# ---- Imports: third parties
+from xlrd import xldate_as_tuple
+from PyQt5.QtCore import QDate
+
+
+def qdate_from_xldate(xldate, datemode=0):
+    """
+    Conver an numerical Excel date to a QDate object
+
+    A value of 0 is used of the workbook was created in Windows (1900-based),
+    while a value of 1 is used if it was created on macOS (1904-based).
+    """
+    date_tuple = xldate_as_tuple(xldate, datemode)
+    return QDate(date_tuple[0], date_tuple[1], date_tuple[2])
