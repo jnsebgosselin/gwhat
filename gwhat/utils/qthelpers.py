92e3d5b22558622cd76a1d9e7e881c4fe8a92ba5
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-09-23 08:47:43 -0400

    Add qthelpers functions qbytearray_to_hexstate and hexstate_to_qbytearray

---
+++ gwhat/utils/qthelpers.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © 2014-2018 GWHAT Project Contributors
+# https://github.com/jnsebgosselin/gwhat
+#
+# This file is part of GWHAT (Ground-Water Hydrograph Analysis Toolbox).
+# Licensed under the terms of the GNU General Public License.
+# -----------------------------------------------------------------------------
+
+"""Qt utilities"""
+
+# ---- Third party imports
+from qtpy.QtCore import QByteArray
+
+
+def qbytearray_to_hexstate(qba):
+    """Convert QByteArray object to a str hexstate."""
+    return str(bytes(qba.toHex().data()).decode())
+
+
+def hexstate_to_qbytearray(hexstate):
+    """Convert a str hexstate to a QByteArray object."""
+    return QByteArray().fromHex(str(hexstate).encode('utf-8'))
