588830c10f911b66106d579cdae07f2b87acb548
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-09-28 12:00:54 -0400

    Add back QToolPanel
    This was deleted by mistake

--- gwhat/common/widgets.py
+++ gwhat/common/widgets.py
@@ -141,3 +141,87 @@ class DialogWindow(QDialog):
             super(DialogWindow, self).show()
 
         self.raise_()
+
+
+class QToolPanel(QWidget):
+    """
+    A custom widget that mimicks the behavior of the "Tools" sidepanel in
+    Adobe Acrobat. It is derived from a QToolBox with the following variants:
+
+    1. Only one tool can be displayed at a time.
+    2. Unlike the stock QToolBox widget, it is possible to hide all the tools.
+    3. It is also possible to hide the current displayed tool by clicking on
+       its header.
+    4. The tools that are hidden are marked by a right-arrow icon, while the
+       tool that is currently displayed is marked with a down-arrow icon.
+    5. Closed and Expanded arrows can be set from custom icons.
+    """
+
+    def __init__(self, parent=None):
+        super(QToolPanel, self).__init__(parent)
+
+        self.__iclosed = QWidget().style().standardIcon(
+            QStyle.SP_ToolBarHorizontalExtensionButton)
+        self.__iexpand = QWidget().style().standardIcon(
+            QStyle.SP_ToolBarVerticalExtensionButton)
+
+        self.setLayout(QGridLayout())
+        self.layout().setContentsMargins(0, 0, 0, 0)  # (l, t, r, b)
+
+        self.__currentIndex = -1
+
+    def setIcons(self, ar_right, ar_down):  # =================================
+        self.__iclosed = ar_right
+        self.__iexpand = ar_down
+
+    def addItem(self, tool, text):  # =========================================
+
+        N = self.layout().rowCount()
+
+        # Add Header :
+
+        head = QPushButton(text)
+        head.setIcon(self.__iclosed)
+        head.clicked.connect(self.__isClicked__)
+        head.setStyleSheet("QPushButton {text-align:left;}")
+
+        self.layout().addWidget(head, N-1, 0)
+
+        # Add Item in a ScrollArea :
+
+        scrollarea = QScrollArea()
+        scrollarea.setFrameStyle(0)
+        scrollarea.hide()
+        scrollarea.setStyleSheet("QScrollArea {background-color:transparent;}")
+        scrollarea.setWidgetResizable(True)
+
+        tool.setObjectName("myViewport")
+        tool.setStyleSheet("#myViewport {background-color:transparent;}")
+        scrollarea.setWidget(tool)
+
+        self.layout().addWidget(scrollarea, N, 0)
+        self.layout().setRowStretch(N+1, 100)
+
+    def __isClicked__(self):  # ===============================================
+
+        for row in range(0, self.layout().rowCount()-1, 2):
+
+            head = self.layout().itemAtPosition(row, 0).widget()
+            tool = self.layout().itemAtPosition(row+1, 0).widget()
+
+            if head == self.sender():
+                if self.__currentIndex == row:
+                    # if clicked tool is open, close it
+                    head.setIcon(self.__iclosed)
+                    tool.hide()
+                    self.__currentIndex = -1
+                else:
+                    # if clicked tool is closed, expand it
+                    head.setIcon(self.__iexpand)
+                    tool.show()
+                    self.__currentIndex = row
+            else:
+                # close all the other tools so that only one tool can be
+                # expanded at a time.
+                head.setIcon(self.__iclosed)
+                tool.hide()
