65840e2cb7d9d2079a586acf5242e52eb427af9b
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-10-01 09:27:49 -0400

    Codestyle

--- gwhat/mainwindow.py
+++ gwhat/mainwindow.py
@@ -273,8 +273,6 @@ def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
 
-# %% if __name__ == '__main__'
-
 if __name__ == '__main__':
     sys.excepthook = except_hook
     main = MainWindow()
