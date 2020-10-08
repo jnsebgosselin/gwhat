5f269fa1c7fc8b89bd4500114e3bf22d1945ae7a
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-10-07 21:53:16 -0400

    Add init file to meteo/tests

---
+++ gwhat/meteo/tests/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+# Copyright © 2014-2018 GWHAT Project Contributors
+# https://github.com/jnsebgosselin/gwhat
+#
+# This file is part of GWHAT (Ground-Water Hydrograph Analysis Toolbox).
+# Licensed under the terms of the GNU General Public License.
