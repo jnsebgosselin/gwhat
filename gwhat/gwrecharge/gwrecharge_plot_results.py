2ff63d0f61a2a39a525dfc816072b36a7ebf7dea
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2021-02-12 15:19:06 -0500

    Update file header

--- gwhat/gwrecharge/gwrecharge_plot_results.py
+++ gwhat/gwrecharge/gwrecharge_plot_results.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
-
-# Copyright © 2014-2018 GWHAT Project Contributors
+# -----------------------------------------------------------------------------
+# Copyright © GWHAT Project Contributors
 # https://github.com/jnsebgosselin/gwhat
 #
 # This file is part of GWHAT (Ground-Water Hydrograph Analysis Toolbox).
 # Licensed under the terms of the GNU General Public License.
+# -----------------------------------------------------------------------------
 
 # ---- Standard library imports
-
 import os
 import os.path as osp
 import datetime
 
 # ---- Imports: third parties
-
 from xlrd.xldate import xldate_from_date_tuple
 import numpy as np
 import matplotlib as mpl
