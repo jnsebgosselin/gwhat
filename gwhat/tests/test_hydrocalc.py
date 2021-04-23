e16b891feec62202f3d3f23954f78c056975800b
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2021-04-23 11:44:25 -0400

    Add test_copy_to_clipboard for the hydrocalc tool

--- gwhat/tests/test_hydrocalc.py
+++ gwhat/tests/test_hydrocalc.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-
-# Copyright © 2014-2018 GWHAT Project Contributors
+# -----------------------------------------------------------------------------
+# Copyright © GWHAT Project Contributors
 # https://github.com/jnsebgosselin/gwhat
 #
 # This file is part of GWHAT (Ground-Water Hydrograph Analysis Toolbox).
 # Licensed under the terms of the GNU General Public License.
+# -----------------------------------------------------------------------------
 
 # ---- Standard Libraries Imports
 import os
@@ -12,7 +13,9 @@ import os.path as osp
 
 # ---- Third Party Libraries Imports
 import pytest
-from PyQt5.QtCore import Qt
+from qtpy.QtCore import Qt
+from qtpy.QtWidgets import QApplication
+
 
 # ---- Local Libraries Imports
 from gwhat.meteo.weather_reader import WXDataFrame
@@ -22,7 +25,9 @@ from gwhat.projet.manager_data import DataManager
 from gwhat.projet.reader_projet import ProjetReader
 
 
+# =============================================================================
 # ---- Pytest Fixtures
+# =============================================================================
 DATADIR = osp.join(osp.dirname(osp.realpath(__file__)), 'data')
 WXFILENAME = osp.join(DATADIR, "MARIEVILLE (7024627)_2000-2015.out")
 WLFILENAME = osp.join(DATADIR, 'sample_water_level_datafile.csv')
@@ -64,9 +69,24 @@ def hydrocalc(datamanager, qtbot):
     return hydrocalc
 
 
-# ---- Test WLCalc
+# =============================================================================
+# ---- Tests
+# =============================================================================
 def test_hydrocalc_init(hydrocalc, mocker):
     assert hydrocalc
+
+
+def test_copy_to_clipboard(hydrocalc, qtbot):
+    """
+    Test that puting a copy of the hydrograph figure on the clipboard is
+    working as expected.
+    """
+    QApplication.clipboard().clear()
+    assert QApplication.clipboard().text() == ''
+    assert QApplication.clipboard().image().isNull()
+
+    qtbot.mouseClick(hydrocalc.btn_copy_to_clipboard, Qt.LeftButton)
+    assert not QApplication.clipboard().image().isNull()
 
 
 if __name__ == "__main__":
