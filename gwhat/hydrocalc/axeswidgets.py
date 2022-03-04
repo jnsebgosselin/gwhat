c5dde7d7a02535f67e1330b114d241c96b3efb75
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2022-03-04 09:03:52 -0500

    Create a new WLCalcVSpanHighlighter

--- gwhat/hydrocalc/axeswidgets.py
+++ gwhat/hydrocalc/axeswidgets.py
@@ -21,6 +21,107 @@ from matplotlib.axes import Axes
 from matplotlib.widgets import AxesWidget
 
 
+class WLCalcVSpanHighlighter(AxesWidget, QObject):
+    sig_span_clicked = QSignal(float)
+
+    def __init__(self, ax: Axes, wlcalc: QWidget, tracked_axvspans: list,
+                 useblit: bool = True, onclicked: Callable = None,
+                 axvspan_color: str = 'red', axvline_color: str = 'black',
+                 ):
+        AxesWidget.__init__(self, ax)
+        QObject.__init__(self)
+        self.visible = True
+        self.useblit = useblit and self.canvas.supports_blit
+        self.wlcalc = wlcalc
+        self.tracked_axvspans = tracked_axvspans
+
+        if onclicked is not None:
+            self.sig_span_clicked.connect(onclicked)
+
+        # Axes span highlight.
+        self.axvspan_highlight = ax.axvspan(
+            0, 1, visible=False, color='red', linewidth=1,
+            ls='-', alpha=0.3)
+
+    def set_active(self, active):
+        """
+        Set whether the selector is active.
+        """
+        self.axvspan_highlight.xy = [
+            [np.inf, 1], [np.inf, 0], [np.inf, 0], [np.inf, 1]]
+        self.axvspan_highlight.set_visible(active)
+        super().set_active(active)
+        self.wlcalc.draw()
+
+    def clear(self):
+        """
+        Clear the selector.
+
+        This method must be called by the canvas BEFORE making a copy of
+        the canvas background.
+        """
+        self.__axvspan_highlight_visible = self.axvspan_highlight.get_visible()
+        self.axvspan_highlight.set_visible(False)
+
+    def restore(self):
+        """
+        Restore the selector.
+
+        This method must be called by the canvas AFTER a copy has been made
+        of the canvas background.
+        """
+        self.axvspan_highlight.set_visible(self.__axvspan_highlight_visible)
+        self.ax.draw_artist(self.axvspan_highlight)
+
+    def onmove(self, event):
+        """Handler to draw the selector when the mouse cursor moves."""
+        if self.ignore(event):
+            return
+        if not self.canvas.widgetlock.available(self):
+            return
+        if not self.visible:
+            return
+
+        if event.xdata is None:
+            self.axvspan_highlight.set_visible(False)
+            self._update()
+            return
+
+        for axvspan in self.tracked_axvspans:
+            if not axvspan.get_visible():
+                continue
+
+            xy_data = axvspan.xy
+            x_data = [xy[0] for xy in xy_data]
+            xdata_min = min(x_data)
+            xdata_max = max(x_data)
+            if event.xdata >= xdata_min and event.xdata <= xdata_max:
+                self.axvspan_highlight.set_visible(True)
+                self.axvspan_highlight.xy = [[xdata_min, 1],
+                                             [xdata_min, 0],
+                                             [xdata_max, 0],
+                                             [xdata_max, 1]]
+                break
+        else:
+            self.axvspan_highlight.set_visible(False)
+        self._update()
+
+    def onpress(self, event):
+        """Handler for the button_press_event event."""
+        if event.button != 1 or not event.xdata:
+            return
+        self.sig_span_clicked.emit(
+            event.xdata - self.wlcalc.dt4xls2mpl * self.wlcalc.dformat)
+        self.onmove(event)
+
+    def onrelease(self, event):
+        pass
+
+    def _update(self):
+        self.ax.draw_artist(self.axvspan_highlight)
+        return False
+
+
 class WLCalcVSpanSelector(AxesWidget, QObject):
     sig_span_selected = QSignal(tuple)
 
