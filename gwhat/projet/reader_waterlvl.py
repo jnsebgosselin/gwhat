f4efea45a589326bf06b6c55baf7f5506dafdd28
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2022-07-14 10:25:06 -0400

    WLDataset: correctly implement setting attrs

--- gwhat/projet/reader_waterlvl.py
+++ gwhat/projet/reader_waterlvl.py
@@ -399,14 +399,17 @@ class WLDataset(WLDatasetBase):
         elif key in COLUMNS:
             return self.data[key].values
         elif key in HEADER.keys():
-            return self._dataf.attrs[key]
+            return self.data.attrs[key]
         elif key == 'filename':
-            return self._dataf.filename
+            return self.data.filename
 
         return self.dset.__getitem__(key)
 
     def __setitem__(self, key, value):
-        self._dataf.attrs[key] = value
+        if key in HEADER.keys():
+            self.data.attrs[key] = value
+        else:
+            raise KeyError(key)
 
     def __load_dataset__(self, filename):
         """Loads the dataset from a file and saves it in the store."""
