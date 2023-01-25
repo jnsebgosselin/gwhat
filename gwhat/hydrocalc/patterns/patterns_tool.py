019490e5d29770bc722bbce6a0807d1bb6f1fb78
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2023-01-25 14:50:53 -0500

    Complet patters tool implementation

--- gwhat/hydrocalc/patterns/patterns_tool.py
+++ gwhat/hydrocalc/patterns/patterns_tool.py
@@ -113,55 +113,33 @@ class SeasonPatternsCalcTool(WLCalcTool, SaveFileMixin):
         #     tip='Save calculated MRC to file.',
         #     triggered=lambda: self.save_mrc_tofile())
 
-        self._select_high_spring_btn = OnOffPushButton(
-            '  Select High Spring', icon='select_range')
-        self._select_high_spring_btn.setToolTip(
+        self._select_highs_btn = OnOffPushButton(
+            '  Select Highs', icon='select_range')
+        self._select_highs_btn.setToolTip(
             'Select periods when maximum water levels were '
-            'reached in the spring.')
-        self._select_high_spring_btn.setCheckable(True)
-        self._select_high_spring_btn.setFocusPolicy(Qt.NoFocus)
-        self._select_high_spring_btn.sig_value_changed.connect(
-            lambda: self._btn_select_high_spring_isclicked())
-
-        self._select_high_fall_btn = OnOffPushButton(
-            '  Select High Fall', icon='select_range')
-        self._select_high_fall_btn.setToolTip(
-            'Select periods when maximum water levels were '
-            'reached in the fall.')
-        self._select_high_fall_btn.setCheckable(True)
-        self._select_high_fall_btn.setFocusPolicy(Qt.NoFocus)
-        self._select_high_fall_btn.sig_value_changed.connect(
-            lambda: self._btn_select_high_fall_isclicked())
+            'reached in the spring or in the fall.')
+        self._select_highs_btn.setCheckable(True)
+        self._select_highs_btn.setFocusPolicy(Qt.NoFocus)
+        self._select_highs_btn.sig_value_changed.connect(
+            lambda: self._btn_select_highs_isclicked())
 
         # Setup the Layout.
         layout = QGridLayout(self)
 
-        layout.addWidget(self._select_high_spring_btn, 0, 0)
-        layout.addWidget(self._select_high_fall_btn, 1, 0)
+        layout.addWidget(self._select_highs_btn, 0, 0)
         layout.setRowMinimumHeight(2, 5)
         layout.setRowStretch(2, 100)
 
     # ---- WLCalc integration
     @wlcalcmethod
-    def _btn_select_high_spring_isclicked(self):
+    def _btn_select_highs_isclicked(self):
         """Handle when the button to select high spring periods is clicked."""
-        if self._select_high_spring_btn.value():
-            self.wlcalc.toggle_navig_and_select_tools(
-                self._select_high_spring_btn)
-        self.high_spring_selector.set_active(
-            self._select_high_spring_btn.value())
+        if self._select_highs_btn.value():
+            self.wlcalc.toggle_navig_and_select_tools(self._select_highs_btn)
+        self.highs_selector.set_active(self._select_highs_btn.value())
 
     @wlcalcmethod
-    def _btn_select_high_fall_isclicked(self):
-        """Handle when the button to select high fall periods is clicked."""
-        if self._select_high_fall_btn.value():
-            self.wlcalc.toggle_navig_and_select_tools(
-                self._select_high_fall_btn)
-        self.high_fall_selector.set_active(
-            self._select_high_fall_btn.value())
-
-    @wlcalcmethod
-    def _on_daterange_selected(self, xldates):
+    def _on_daterange_selected(self, xldates, button):
         """
         Handle when a new period of high spring or hign fall water levels is
         selected by the user.
@@ -188,82 +166,21 @@ class SeasonPatternsCalcTool(WLCalcTool, SaveFileMixin):
         mask = (data.index >= dtmin) & (data.index <= dtmax)
         argmin = np.argmin(data['WL'][mask])
 
-        if self._select_high_spring_btn.value():
+        if button == 1:
             high_type = 'high_spring'
-        elif self._select_high_fall_btn.value():
+        elif button == 3:
             high_type = 'high_fall'
 
         self._feature_points[high_type][
             data.index[mask][argmin]
             ] = data['WL'][mask][argmin]
 
-        self.feature_points_plotter.set_feature_points(self._feature_points)
-        self.wlcalc.update_axeswidgets()
-
-    # @wlcalcmethod
-    # def _draw_mrc(self):
-    #     """
-    #     Draw the periods during which water levels recedes and draw the
-    #     water levels that were predicted with the MRC.
-    #     """
-    #     self._draw_mrc_wl()
-    #     self._draw_mrc_periods()
-    #     self.wlcalc.draw()
-
-    # @wlcalcmethod
-    # def _draw_mrc_wl(self):
-    #     """Draw the water levels that were predicted with the MRC."""
-    #     if (self.wldset is not None and
-    #             self.btn_show_mrc.value() and
-    #             self.wldset.mrc_exists()):
-    #         self._mrc_plt.set_visible(True)
-    #         mrc_data = self.wldset.get_mrc()
-
-    #         x = mrc_data['time'] + self.wlcalc.dt4xls2mpl * self.wlcalc.dformat
-    #         y = mrc_data['recess']
-    #         self._mrc_plt.set_data(x, y)
-    #     else:
-    #         self._mrc_plt.set_visible(False)
-
-    # @wlcalcmethod
-    # def _btn_del_period_isclicked(self):
-    #     """Handle when the button btn_del_period is clicked."""
-    #     if self.btn_del_period.value():
-    #         self.wlcalc.toggle_navig_and_select_tools(self.btn_del_period)
-    #         self.btn_show_mrc.setValue(True)
-    #     self.mrc_remover.set_active(self.btn_del_period.value())
-    #     self.wlcalc.draw()
+        self._draw_patterns_feature_points()
 
     @wlcalcmethod
     def _draw_patterns_feature_points(self):
-        pass
-    #     """Draw the periods that will be used to compute the MRC."""
-    #     self.btn_undo.setEnabled(len(self._mrc_period_memory) > 1)
-    #     for axvspan in self._mrc_period_axvspans:
-    #         axvspan.set_visible(False)
-    #     if self.wldset is not None and self.btn_show_mrc.value():
-    #         for i, xdata in enumerate(self._mrc_period_xdata):
-    #             xmin = xdata[0] + (
-    #                 self.wlcalc.dt4xls2mpl * self.wlcalc.dformat)
-    #             xmax = xdata[1] + (
-    #                 self.wlcalc.dt4xls2mpl * self.wlcalc.dformat)
-    #             try:
-    #                 axvspan = self._mrc_period_axvspans[i]
-    #                 axvspan.set_visible(True)
-    #                 axvspan.xy = [[xmin, 1], [xmin, 0],
-    #                               [xmax, 0], [xmax, 1]]
-    #             except IndexError:
-    #                 axvspan = self.wlcalc.fig.axes[0].axvspan(
-    #                     xmin, xmax, visible=True, color='red', linewidth=1,
-    #                     ls='-', alpha=0.1)
-    #                 self._mrc_period_axvspans.append(axvspan)
-
-    # def btn_show_mrc_isclicked(self):
-    #     """Handle when the button to draw of hide the mrc is clicked."""
-    #     if self.btn_show_mrc.value() is False:
-    #         self.btn_add_period.setValue(False)
-    #         self.btn_del_period.setValue(False)
-    #     self._draw_mrc()
+        self.feature_points_plotter.set_feature_points(self._feature_points)
+        self.wlcalc.update_axeswidgets()
 
     # ---- WLCalcTool API
     def is_registered(self):
@@ -275,25 +192,18 @@ class SeasonPatternsCalcTool(WLCalcTool, SaveFileMixin):
 
         index = wlcalc.tools_tabwidget.addTab(self, self.title())
         wlcalc.tools_tabwidget.setTabToolTip(index, self.tooltip())
-        # wlcalc.sig_date_format_changed.connect(self._draw_mrc)
 
         # Setup the axes widget to select high water level periods.
-        wlcalc.register_navig_and_select_tool(self._select_high_spring_btn)
-        wlcalc.register_navig_and_select_tool(self._select_high_fall_btn)
+        wlcalc.register_navig_and_select_tool(self._select_highs_btn)
 
         # Setup the selectors for the periods of high spring and high fall
         # water levels.
-        self.high_spring_selector = WLCalcVSpanSelector(
+        self.highs_selector = WLCalcVSpanSelector(
             self.wlcalc.fig.axes[0], wlcalc,
             onselected=self._on_daterange_selected,
-            axvspan_color='green', axvline_color='green')
-        wlcalc.install_axeswidget(self.high_spring_selector)
-
-        self.high_fall_selector = WLCalcVSpanSelector(
-            self.wlcalc.fig.axes[0], wlcalc,
-            onselected=self._on_daterange_selected,
-            axvspan_color='orange', axvline_color='orange')
-        wlcalc.install_axeswidget(self.high_fall_selector)
+            axvspan_colors=['green', 'orange'],
+            allowed_buttons=[1, 3])
+        wlcalc.install_axeswidget(self.highs_selector)
 
         # Setup the seasonal pattern feature points plotter.
         self.feature_points_plotter = FeaturePointPlotter(
