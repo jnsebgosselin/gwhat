6cdfa883c6a668c59c0e0209a002ccee62fb154f
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2023-03-07 08:35:24 -0500

    Extend qthelpers to be able to pass an string as an icon

--- gwhat/utils/qthelpers.py
+++ gwhat/utils/qthelpers.py
@@ -10,4 +10,31 @@
 """Extension of the Qt helpers module."""
 
 from apputils.qthelpers import *
-import gwhat.utils.icons
+import apputils.qthelpers
+
+# ---- Local library imports
+from gwhat.utils.icons import get_icon
+
+
+def create_toolbutton(*args, **kwargs):
+    """
+    Extend create_toolbutton so that we can pass the name of an
+    icon instead of a QIcon object.
+    """
+    if len(args) >= 3 and isinstance(args[4], str):
+        args[4] = get_icon(args[4])
+    if 'icon' in kwargs and isinstance(kwargs['icon'], str):
+        kwargs['icon'] = get_icon(kwargs['icon'])
+    return apputils.qthelpers.create_toolbutton(*args, **kwargs)
+
+
+def create_action(*args, **kwargs):
+    """
+    Extend create_action so that we can pass the name of an
+    icon instead of a QIcon object.
+    """
+    if len(args) >= 3 and isinstance(args[4], str):
+        args[4] = get_icon(args[4])
+    if 'icon' in kwargs and isinstance(kwargs['icon'], str):
+        kwargs['icon'] = get_icon(kwargs['icon'])
+    return apputils.qthelpers.create_action(*args, **kwargs)
