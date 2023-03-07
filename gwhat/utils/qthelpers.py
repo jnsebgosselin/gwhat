bcdbaeb43001c793cec5cc6fb8c917f242e08f56
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2023-03-07 08:53:19 -0500

    Update qthelpers.py

--- gwhat/utils/qthelpers.py
+++ gwhat/utils/qthelpers.py
@@ -10,31 +10,7 @@
 """Extension of the Qt helpers module."""
 
 from apputils.qthelpers import *
-import apputils.qthelpers
 
-# ---- Local library imports
-from gwhat.utils.icons import get_icon
-
-
-def create_toolbutton(*args, **kwargs):
-    """
-    Extend create_toolbutton so that we can pass the name of an
-    icon instead of a QIcon object.
-    """
-    if len(args) >= 3 and isinstance(args[4], str):
-        args[4] = get_icon(args[4])
-    if 'icon' in kwargs and isinstance(kwargs['icon'], str):
-        kwargs['icon'] = get_icon(kwargs['icon'])
-    return apputils.qthelpers.create_toolbutton(*args, **kwargs)
-
-
-def create_action(*args, **kwargs):
-    """
-    Extend create_action so that we can pass the name of an
-    icon instead of a QIcon object.
-    """
-    if len(args) >= 3 and isinstance(args[4], str):
-        args[4] = get_icon(args[4])
-    if 'icon' in kwargs and isinstance(kwargs['icon'], str):
-        kwargs['icon'] = get_icon(kwargs['icon'])
-    return apputils.qthelpers.create_action(*args, **kwargs)
+# We need to import the 'icons' module here so that the list of
+# avail icons in 'apputils.icons' get expanded correctly.
+import gwhat.utils.icons
