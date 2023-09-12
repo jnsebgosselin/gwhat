6d43843ed21f845cb8503b4ea0680f6e16eb7ad3
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2023-09-12 08:57:19 -0400

    Update script_venv_setup.bat

--- script_venv_setup.bat
+++ script_venv_setup.bat
@@ -4,5 +4,5 @@ python -m pip install --upgrade pip
 python -m pip install wheel
 python -m pip install cython>=0.25.2 numpy==1.21.*
 python -m pip install -e ../gwhat
-python -m pip install spyder-kernels==2.3.*
+python -m pip install spyder-kernels==2.4.*
 pause
